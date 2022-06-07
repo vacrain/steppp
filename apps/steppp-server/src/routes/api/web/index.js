ab61df0785270cf8074e2793dca399ec69af1313
vacrain <726866011@qq.com>
2022-06-07 19:48:12 +0800

    refactor(projects): 移除apps

--- apps/steppp-server/src/routes/api/web/index.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default async function genresFn(fastify, opts) {
-  fastify.get('/test', async function genresRoot() {
-    return 'welcome! test web-api root success~'
-  })
-}
--- apps/steppp-server/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default async function genresFn(fastify, opts) {
-  fastify.get('/', async function genresRoot() {
-    return 'here is api root '
-  })
-}
