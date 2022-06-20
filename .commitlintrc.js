60c35ae100aa5d21c781aa787896ec9e4a8e54bb
vacrain <726866011@qq.com>
2022-06-20 11:54:26 +0800

    :building_construction: chore(whole): 调整package以及commit设置,表情提交又回来啦:tada:

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,29 +1,30 @@
 /*
  * Author        vacrain
  * CreateDate    2022-06-02
- * LastEditor    Vacrain
- * lastEditDate  2022-06-11
+ * LastEditor    vacrain
+ * lastEditDate  2022-06-20
  * RelativePath  /.commitlintrc.js
  * Description   提交校验配置
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
 
+  // config from commitlint-config-git-commit-emoji
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(?<type>.*\s\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))$/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
   // 遵循Angular规范:
   //    header: <type>[(<scope)]: <subject>
   //    body:...
   //    footer:...
   rules: {
-    // 都有哪些type
-    'type-enum': [
-      2,
-      'always',
-      // 下列type的含义请查看 ./.cz-config.js
-      ['feat', 'fix', 'docs', 'style', 'perf', 'refactor', 'chore', 'release', 'revert', 'test', 'ci'],
-    ],
-
-    // header 部分
-    'header-max-length': [2, 'never', 72], // header 最长72
+    // 每一项rule后的数组：
+    // 第一位为level，可选0,1,2，0为disable，1为warning，2为error
+    // 第二位为应用与否，可选always|never
+    // 第三位该rule的值
 
     //  -case参考,可选值如下
     // 'lower-case' 小写 lowercase
@@ -35,23 +36,35 @@ module.exports = {
     // 'snake-case' 下划线 snake_case
     // 'start-case' 所有首字母大写 start-case
 
-    // <type> 部分
+    'header-max-length': [2, 'always', 72],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+
+    'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'], // <type> 不能为空
-    'type-case': [2, 'always', 'lower-case'], // <type>格式小写
-
-    // <scope> 部分
-    // 'scope-empty': [2, 'never'], // <scope> 不能为空
-    'scope-case': [2, 'always', 'lower-case'], // <scope> 格式 小写
-
-    // <subject> 部分
-    'subject-empty': [2, 'never'], // <subject> 不能为空
+    'type-enum': [
+      2,
+      'always',
+      // 下列type的含义请查看 ./.cz-config.js
+      // ['feat', 'fix', 'docs', 'style', 'perf', 'refactor', 'chore', 'release', 'revert', 'test', 'ci'],
+      [
+        ':sparkles: feat',
+        ':bug: fix',
+        ':memo: docs',
+        ':lipstick: style',
+        ':zap: perf',
+        ':recycle: refactor',
+        ':building_construction: chore',
+        ':rocket: release',
+        ':rewind: revert',
+        ':white_check_mark: test',
+        ':construction_worker: ci',
+      ],
+    ],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-exclamation-mark': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'], // <subject> 以.为结束标志
-    'subject-case': [2, 'never', []],
-
-    // body 部分
-    'body-leading-blank': [2, 'always'], // body换行
-
-    // footer 部分
-    'footer-leading-blank': [1, 'always'], // <footer> 以空行开头
   },
 }
--- .cz-config.js
+++ .cz-config.js
@@ -1,41 +1,51 @@
 /*
  * Author        vacrain
  * CreateDate    2022-06-02
- * LastEditor    Vacrain
- * lastEditDate  2022-06-11
+ * LastEditor    vacrain
+ * lastEditDate  2022-06-20
  * RelativePath  /.cz-config.js
  * Description   cz工具配置
  * 参考：https://zhuanlan.zhihu.com/p/69635847
  */
 module.exports = {
   types: [
-    // 修改了的话，需要同步修改 ./.commitlintrc.js ！！！
-    { value: 'feat', name: '✨ feat:       新增功能' },
-    { value: 'fix', name: '🐛 fix:        修复bug' },
-    { value: 'docs', name: '✏️  docs:       文档变更' },
-    { value: 'style', name: '💄 style:      代码格式' },
-    { value: 'perf', name: '⚡️ perf:       性能优化' },
-    { value: 'refactor', name: '♻️  refactor:   代码重构（不包括 bug 修复、功能新增）' },
-    { value: 'chore', name: '🏗  chore:      构建或辅助工具的变动（不影响源文件、测试用例）' },
-    { value: 'release', name: '🚀 release:    发布新版本' },
-    { value: 'revert', name: '⏪ revert:     回滚 commit' },
-    { value: 'test', name: '🤺 test:       测试用例变更' },
-    { value: 'ci', name: '🎡 ci:         修改 CI 配置、脚本' },
+    // 修改了的话，需要同步修改 ./.commitlintrc.js 中的type-enum ！！！
+    { value: ':sparkles: feat', name: '✨ feat:       新增功能' },
+    { value: ':bug: fix', name: '🐛 fix:        修复bug' },
+    { value: ':memo: docs', name: '📝 docs:       文档变更' },
+    { value: ':lipstick: style', name: '💄 style:      代码格式' },
+    { value: ':zap: perf', name: '⚡️ perf:       性能优化' },
+    { value: ':recycle: refactor', name: '♻️  refactor:   代码重构（不包括 bug 修复、功能新增）' },
+    { value: ':building_construction: chore', name: '🏗️  chore:      构建或辅助工具的变动（不影响源文件、测试用例）' },
+    { value: ':rocket: release', name: '🚀 release:    发布新版本' },
+    { value: ':rewind: revert', name: '⏪ revert:     回滚 commit' },
+    { value: ':white_check_mark: test', name: '✅ test:       测试用例变更' },
+    { value: ':construction_worker: ci', name: '👷 ci:         修改 CI 配置、脚本' },
   ],
   scopes: [
     ['whole', '整体都会受影响'],
-    ['demos', '代码学习与实践'],
-    ['apps', '应用开发'],
-    ['packages', '应用共用'],
-    ['todo', '计划相关'],
-    ['other', '其他修改'],
+    ['app', '应用开发'],
+    ['demo', '代码学习与实践'],
+    ['package', '公用依赖'],
     ['custom', '以上都不是？我要自定义'],
   ].map(([value, description]) => {
     return {
       value,
-      name: `${value.padEnd(30)} (${description})`,
+      name: `${value.padEnd(15)} ${description}`,
     }
   }),
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+  scopeOverrides: {
+    fix: [
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },
+  */
   messages: {
     type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
     scope: '\n选择一个 scope（可选）：',
@@ -47,7 +57,7 @@ module.exports = {
     confirmCommit: '确认提交？',
   },
 
-  allowBreakingChanges: ['feat', 'fix'], // 填写的type，会询问填写breaking（列举非兼容性重大的变更），不写就不会问
+  allowBreakingChanges: [':sparkles: feat', ':bug: fix'], // 数组中的type会询问填写breaking（列举非兼容性重大的变更）
   subjectLimit: 66, // subject 最大长度
   breaklineChar: '|', // break line
 }
